4e00d396b6bbc7324f8782075f1d72a8e5e118ee
Terry <misterfreemann@gmail.com>
2023-05-27 14:29:18 -0400

    created typeDefs and mutations

---
+++ server/Schemas/resolvers.js
@@ -0,0 +1,45 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require("../models");
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        users: async () => {
+            return User.find({})
+        },
+        user: async (parent, { userId }) => {
+            return User.findOne({ _id: userId })
+        },
+        me: async (parent, args, context) => {
+            if (context.User) {
+                return User.findOne({ _id: context.user._id })
+            }
+            throw new AuthenticationError('You need to be logged in')
+        }
+    },
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password })
+            const token = signToken(username)
+
+            return { token, username }
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('No profile with this email found!')
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+            if (!password) {
+                throw new AuthenticationError('Incorrect password!')
+            }
+            const token = signToken(user)
+            return { token, user };
+        }
+    }
+
+
+
+
+}
