af6db72ab306116aab2fe350f7521cc6345caf76
Terry <misterfreemann@gmail.com>
2023-06-01 14:42:45 -0400

    deploying fourth time

--- server/server.js
+++ server/server.js
@@ -19,14 +19,14 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(routes);
+// app.use(routes);
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'))
-  });
 }
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+});
 
 // starting apollo server
 const startApolloServer = async () => {
